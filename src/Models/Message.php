9187e3b5c34d0f8c343227f821ba3def3eaee29e
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2017-01-19 20:34:56 +0100

    Updating

---
+++ src/Models/Message.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AcachaScool\Social\Internalmsn\Models;
+
+use Illuminate\Queue\SerializesModels;
+
+/**
+ * Created by PhpStorm.
+ * User: sylver
+ * Date: 12/01/17
+ * Time: 19:51
+ */
+class Message extends Model
+{
+
+}
