7738f76d2a573e895caecf6bd7fe176c19422f6f
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2017-01-19 21:09:29 +0100

    Solved problems with the terminal

--- src/Models/Message.php
+++ src/Models/Message.php
@@ -4,12 +4,6 @@ namespace AcachaScool\Social\Internalmsn\Models;
 
 use Illuminate\Queue\SerializesModels;
 
-/**
- * Created by PhpStorm.
- * User: sylver
- * Date: 12/01/17
- * Time: 19:51
- */
 class Message extends Model
 {
 
